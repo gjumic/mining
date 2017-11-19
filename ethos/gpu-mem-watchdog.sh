bb87ad27ce5b5cbb08935deaf84ef18548c0e4d9
gjumic <jumic.goran@gmail.com>
2017-11-19 19:15:39 +0100

    Update gpu-mem-watchdog.sh

--- ethos/gpu-mem-watchdog.sh
+++ ethos/gpu-mem-watchdog.sh
@@ -3,17 +3,16 @@
 
 NOWT=$(date +"%D"-"%T")
 echo "Sleeping 280 seconds, just in case!"
-sleep 280
-/opt/ethos/bin/update| grep -w mem>output.file
-MEMORY="$( awk '$3 < 1000 {print $0;}' output.file )"
+#sleep 280
+/opt/ethos/bin/update | grep -w mem > /opt/tools/output.file
+MEMORY="$( awk '$3 < 1000 {print $0;}' /opt/tools/output.file )"
 echo $MEMORY
-	if  [[ -z $MEMORY ]]; 
-	then 
-		echo "None of the cards have less than 1000 mem speed"
-		echo "GPU CHECK OK! $NOWT">>/root/rebooted.log
-	else
-		echo "$MEMORY" >> /root/rebooted.log 
-		echo "GPU DOWN $NOWT -> REBOOT">>/root/rebooted.log 
-		/opt/ethos/bin/hard-reboot
-	fi
- 
+        if  [[ -z $MEMORY ]];
+        then
+                echo "None of the cards have less than 1000 mem speed"
+                echo "GPU CHECK OK! $NOWT" >> /opt/tools/rebooted.log
+        else
+                echo "$MEMORY" >> /opt/tools/rebooted.log
+                echo "GPU DOWN $NOWT -> REBOOT" >> /opt/tools/rebooted.log
+                /opt/ethos/bin/hard-reboot
+fi
