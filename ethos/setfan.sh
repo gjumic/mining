c18dce03c694c9e1b2b56da9f79c0a0bb9b709a4
gjumic <jumic.goran@gmail.com>
2017-12-27 13:40:20 +0100

    Update setfan.sh

--- ethos/setfan.sh
+++ ethos/setfan.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-
+######################################################
+# README - SETFAN for AMD gpus v1.0 by Stormer
+######################################################
+# FOR AMD CARDS ONLY
+# Just put this script and give it execute rights with chmod
+# Execute script with with how many percent you want fans to spin all gpus (./setfan.sh 50)
+######################################################
+# If you have found this script useful please donate BTC or ETH to following adresses.
+# This will give me more motivation to work on this and many more scripts:
+# BTC = 1Dqa4Exdc2cfeMuhZ7Pnf9ri253UtbhsxY
+# ETH = 0xe42fb03f179Fe4e11480D623e5C40eA070a6222F
+######################################################
 GPUCOUNT=$(cat /var/run/ethos/gpucount.file)
 SETFAN=$1
 
